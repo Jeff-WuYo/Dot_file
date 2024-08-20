86295a6b6435cf5b373d5aa8791aa7ed45f553d5
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2024-08-21 00:22:35 +0800

    update nvim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,8 +1,26 @@
-vim.o.bg = "light"
+if os.getenv('TERM') == 'linux' then
+  vim.o.bg = 'dark'
+else
+  vim.o.bg = 'light'
+end
 vim.o.nu = true
 vim.o.rnu = true
 vim.o.cursorline = true
-vim.o.tabstop = 2
+vim.o.tabstop = 4
 vim.o.shiftwidth = 2
-vim.o.tabstop = 2
+vim.o.softtabstop = 2
 vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.hlsearch = false
+vim.o.incsearch = true
+vim.o.scrolloff = 8
+vim.o.updatetime = 50
+vim.cmd.colorscheme('vim')
+vim.o.tgc = false
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+--vim.g.mapleader = " "
+--vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+--vim.o.signcolumn = 'yes'
